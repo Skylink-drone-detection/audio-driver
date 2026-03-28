f3eb92eb7f702ee10e81d58e63f4e9229aeccbb7
maikelele <lahcim.md@gmail.com>
2026-03-28 03:14:00 +0100

    Add simple logging

--- main.c
+++ main.c
@@ -99,7 +99,7 @@ int main(void){
 
 	
 	while(true) {
-        printf("1\n");
+        printf("Reading all channels\n");
     	read_all_channels(channel_values); // Read initial values to ensure everything is working
 		++loop_counter;
 		if(shm_write(my_buffer, channel_values) != 0){ // Write ADC values to shared memory for other processes to read
@@ -109,7 +109,7 @@ int main(void){
 
 		if (wav_enabled && wav_frames_written < wav_frames_target) {
 			for (uint16_t ch = 0; ch < wav_channels; ++ch) {
-                printf("Voltage to PCM16");
+                printf("Voltage to PCM16\n");
 				wav_frame_i16[ch] = voltage_to_pcm16(channel_values[ch]);
                 fprintf(stdout, "Data written: %d\n", wav_frame_i16[ch]);
 			}
